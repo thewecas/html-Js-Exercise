55f085a3519f4129a88f6f6c9782edcc0145f087
Vikas Shettigar <75922101+thewecas@users.noreply.github.com>
2023-07-18 17:34:18 +0530

    Update 11.js

--- 11.js
+++ 11.js
@@ -1,21 +1,10 @@
-// const nearestLargerValue = (arr, index) => {
-//   return arr.filter((ele, i) => {
-//     return i > index && ele > arr[index];
-//   })[0];
-// };
-
-// const nearestLargerValue = (arr, index) => {
-//   for (let i = index + 1; i < arr.length; i++)
-//     if (arr[i] > arr[index]) return arr[i];
-//   return null;
-// };
-
-const nearestLargerValue = (arr, index) => {
+const nearestLargerValue = (arr, val) => {
   let dist = Number.MAX_VALUE;
   let largerVal = null;
+  let index = arr.indexOf(val);
 
   arr.forEach((ele, i) => {
-    if (ele > arr[index] && Math.abs(i - index) < dist) {
+    if (ele > val && Math.abs(i - index) < dist) {
       dist = Math.abs(i - index);
       largerVal = ele;
     }
@@ -26,7 +15,6 @@ const nearestLargerValue = (arr, index) => {
 
 const arr = [1, 4, 3, 2, 5, 7];
 
-console.log(nearestLargerValue(arr, 2)); // Output : 4
-console.log(nearestLargerValue(arr, 3)); // Output : 3
-
-console.log("\n\n");
+console.log(nearestLargerValue(arr, 2)); // Output : 3
+console.log(nearestLargerValue(arr, 3)); // Output : 4
+console.log(nearestLargerValue(arr, 4)); // Output : 5
